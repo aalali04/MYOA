98aa7ba2ad7450496cf1561df93f5efd8f9b2012
Alaa Alali <aalali04@gmail.com>
2019-09-13 10:42:12 +0300

    creating the xcode file for MYOA

---
+++ MYOA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MYOA
+//
+//  Created by Alaa Alali on 13/09/2019.
+//  Copyright © 2019 Alaa Alali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
